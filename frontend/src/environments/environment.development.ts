415797fdce05c873b0804908daee78bfc05b679b
ognjen <79698642+komadiina@users.noreply.github.com>
2025-08-06 23:41:03 +0200

    Update environment.development.ts

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,11 +1,7 @@
 export const environment: {
 	production: boolean;
-	googleClientID: string;
-	googleAPIKey: string;
 	backendUrl: string;
 } = {
 	production: false,
-	googleClientID: '123456789.apps.googleusercontent.com',
-	googleAPIKey: '123456789',
-	backendUrl: 'http://localhost:4300',
+	backendUrl: 'http://localhost:4201',
 };
